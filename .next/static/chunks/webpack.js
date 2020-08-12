2c5b111be9df171a665fbf80250dbac1a7bbb36e
Thomas Helderop <thomashelderop@Thomass-MacBook-Air.local>
2020-08-12 14:17:20 -0600

    topics page layout

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "badccc767b6eff00d399";
+/******/ 	var hotCurrentHash = "8cd14d42aaaa6f3acff1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
