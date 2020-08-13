d85fee354cd88926a4015de43efe835767ce2844
Thomas Helderop <thomashelderop@Thomass-MacBook-Air.local>
2020-08-12 23:02:09 -0600

    code doesn't work, but i'm using DOM manipulation to reveal the sent message

--- pages/chat.js
+++ pages/chat.js
@@ -1,12 +1,21 @@
 function send() {
     return (
-        
+        document.getElementsByClassName("first-message").hidden = false
     )
 }
 
 export default function Chat() {
     return (
         <div>
+
+            <div className="first-message" hidden>
+                <p>I thought this article was interesting!</p>
+            </div>
+
+            <div className="second-message" hidden>
+
+            </div>
+
             <form>
                 <textarea>Hello! I am filler text. Nice to meet you!</textarea>
                 <button onClick={send()}>Send</button>
