dcdce18747e5a88bcf28f005412f4c91b760dbd9
Thomas Helderop <thomashelderop@Thomass-MacBook-Air.local>
2020-08-13 10:50:07 -0600

    fixing github and next stuff

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/chat":["static\u002Fchunks\u002Fpages\u002Fchat.js"],"/conversation-topics":["static\u002Fchunks\u002Fpages\u002Fconversation-topics.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
