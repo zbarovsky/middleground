92dd2d2a27848f798ff9a9a357b16e58acca58a7
Thomas Helderop <thomashelderop@Thomass-MacBook-Air.local>
2020-08-13 11:39:48 -0600

    further chat page styling

--- pages/chat.js
+++ pages/chat.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 import Participants from './chat-participants'
+import Navbar from '../components/navbar'
 
 export default function Chat() {
 
@@ -16,26 +17,29 @@ export default function Chat() {
     })
 
     return (
-        <div class="container">
-            <div class="row">
-                <div class="col-sm">
-                    <Participants />
-                    <Participants />
-                </div>
-                <div class="col-lg .offset-md-4">
-                    <div className={styles.firstMessage} id="first-message">
-                        <p>I thought this article was interesting!</p>
+        <div>
+            <Navbar />
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm">
+                        <Participants />
+                        <Participants />
                     </div>
+                    <div class="col-lg .offset-md-4">
+                        <div className={styles.firstMessage} id="first-message">
+                            <p>I thought this article was interesting!</p>
+                        </div>
 
-                    <div className={styles.secondMessage}>
-                        <p>So did I!</p>
+                        <div className={styles.secondMessage}>
+                            <p>So did I!</p>
+                        </div>
+
+
+                        <form className={styles.chatbox}>
+                            <textarea placeholder="Hello! I am filler text. Nice to meet you!"></textarea>
+                            <button onClick={useEffect}>Send</button>
+                        </form>
                     </div>
-
-
-                    <form className={styles.chatbox}>
-                        <textarea placeholder="Hello! I am filler text. Nice to meet you!"></textarea>
-                        <button onClick={useEffect}>Send</button>
-                    </form>
                 </div>
             </div>
         </div>
