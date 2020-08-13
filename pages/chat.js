da02291794af608a51d6ab7b8259fab352fd91ee
Thomas Helderop <thomashelderop@Thomass-MacBook-Air.local>
2020-08-13 08:27:46 -0600

    getting closer to making it work

--- pages/chat.js
+++ pages/chat.js
@@ -3,20 +3,22 @@ import styles from '../styles/Home.module.css'
 
 export default function Chat() {
 
-    // useEffect(() => {
-    //     document.getElementsByClassName("firstMessage").styles.display = "block"
-    // }, [])
+    useEffect(() => {
+        document.getElementById("first-message").style.display = "block"
+    })
 
     return (
         <div>
+            <div>
+                <div className={styles.firstMessage} id="first-message">
+                    <p>I thought this article was interesting!</p>
+                </div>
 
-            <div className={styles.firstMessage}>
-                <p>I thought this article was interesting!</p>
+                <div className={styles.secondMessage}>
+                    <p>So did I!</p>
+                </div>
             </div>
 
-            <div className={styles.secondMessage}>
-
-            </div>
 
             <form>
                 <textarea>Hello! I am filler text. Nice to meet you!</textarea>
