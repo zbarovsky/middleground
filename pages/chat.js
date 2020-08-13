cc156065f0e00a0ce1b66331054bb8be878201d6
Thomas Helderop <thomashelderop@Thomass-MacBook-Air.local>
2020-08-13 09:07:05 -0600

    started styling chat page

--- pages/chat.js
+++ pages/chat.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import styles from '../styles/Home.module.css'
+import Participants from './chat-participants'
 
 export default function Chat() {
 
@@ -16,21 +17,41 @@ export default function Chat() {
 
     return (
         <div>
-            <div>
-                <div className={styles.firstMessage} id="first-message">
-                    <p>I thought this article was interesting!</p>
-                </div>
-
-                <div className={styles.secondMessage}>
-                    <p>So did I!</p>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm">
+                        <Participants />
+                        <Participants />
+                    </div>
                 </div>
             </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg">
+                        <div className={styles.firstMessage} id="first-message">
+                            <p>I thought this article was interesting!</p>
+                        </div>
+
+                        <div className={styles.secondMessage}>
+                            <p>So did I!</p>
+                        </div>
 
 
-            <form>
-                <textarea>Hello! I am filler text. Nice to meet you!</textarea>
-                <button onClick={useEffect}>Send</button>
-            </form>
+                        <form>
+                            <textarea>Hello! I am filler text. Nice to meet you!</textarea>
+                            <button onClick={useEffect}>Send</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
+
+<div class="container">
+<div class="row">
+    <div class="col-sm">
+        <Participants />
+    </div>
+    </div>
+    </div>
